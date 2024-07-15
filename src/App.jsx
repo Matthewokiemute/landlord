a9aa8728b9b0bc8d97402a024657b0ae6b4d02e4
Matthewokimute <ego.okiemute@gmail.com>
2024-07-15 15:17:16 +0100

    feat: create pages and routes > installed react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-
   return (
-    <h1 className='text-red-400'>Landlord App</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
