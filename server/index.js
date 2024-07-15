5410092308755d62db003c5ddd47b633a5598bc8
Matthewokimute <ego.okiemute@gmail.com>
2024-07-15 18:27:36 +0100

    create middleware and functions to handle errors

--- server/index.js
+++ server/index.js
@@ -6,13 +6,13 @@ import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => {
-    console.log('Connected to MongoDB!');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(process.env.MONGODB_URL)
+    .then(() => {
+        console.log('Connected to MongoDB!');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 //   const __dirname = path.resolve();
 
@@ -25,3 +25,14 @@ app.listen(3000, () => console.log("Server is running on port 3000!"))
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
